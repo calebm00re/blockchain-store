019d500b04cc46f67c944489bbd48885870fd8c9
Caleb Moore <ctmoore@smu.edu>
2022-12-02 11:30:34 -0600

    scrapping old path

--- backend.py
+++ backend.py
@@ -7,32 +7,13 @@ from secrets import Secrets
 import json
 
 
-def get_transaction_hex(price, gas, prev_idx, prev_tx, change_addy=None):
-    prev_tx = bytes.fromhex(prev_tx)
-    secrets = Secrets()
-
-    if change_addy is None:
-        change_addy = secrets.get_address()
-
-    tx_ins = []
-    tx_ins.append(TxIn(prev_tx, prev_idx))
-
-    tx_outs = []
-    h160 = decode_base58(secrets.get_address())
-
-    script_pubkey = p2pkh_script(h160)
-    target_satoshis = int(price*100000000)
-    tx_outs.append(TxOut(target_satoshis, script_pubkey))
-    h160 = decode_base58(change_addy)
-    script_pubkey = p2pkh_script(h160)
-    change_satoshis = int((price-gas)*100000000)
-    tx_outs.append(TxOut(change_satoshis, script_pubkey))
-
-    tx_obj = Tx(1, tx_ins, tx_outs, 0, testnet=True)
-
-    print(tx_obj.sign_input(0, secrets.get_priv()))
-
-    print(tx_obj.serialize().hex())
+def get_transaction_hex(price=None, gas=None, target_addy=None, change_addy=None):
+    s = Secrets()
+    store_addy = str(s.get_address())
+    url = 'https://api.blockcypher.com/v1/btc/test3/addrs/' + store_addy + '/full?limit=50'
+    data = urlopen(url)
+    data = data.readlines()
+    print(data)
     return
 
-get_transaction_hex(.0003, .0001, 1, 'c184a5551747beda8f676f84dd6e91da550482e9fd18e65f95a9b5fb399f18b2')
+get_transaction_hex()

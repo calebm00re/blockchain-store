5193469ce3fd2641363d584c9d96b35dbe39de33
Caleb Moore <ctmoore@smu.edu>
2022-12-07 14:15:04 -0600

    deployment push

--- pages/1_Jeep.py
+++ pages/1_Jeep.py
@@ -3,23 +3,27 @@ from verification import verify_payment
 import time
 import webbrowser
 
-price = '50,000'
+price = '1,000'
 st.text('Jeep')
 st.image('https://techcrunch.com/wp-content/uploads/2022/09/JP023_279WR5jvo53mde21dbrr902p5rahl1e.jpg')
 addy = st.text_input('Enter your purchasing address to confirm transaction')
-email = st.text_input('How should we contact you?')
+contact_type = st.selectbox('How would you like to arrange delivery', ('select', 'Email', 'Phone'))
+contact = st.text_input('Contact' if contact_type == 'select' else contact_type)
 st.text(price)
 btn = st.button('Confirm')
 if btn:
     cost = int(price.replace(',', ''))
-    res = verify_payment(cost, addy)
-    with st.spinner('Scanning our store wallet...'):
-        time.sleep(3)
-    if res['result']:
-        st.balloons()
-        st.success(res['reason'], icon="✅")
-    else:
-        if res['severity'] == 1:
-            st.warning(res['reason'], icon='⚠️')
+    if addy != '' and contact_type != 'select' and contact != '':
+        res = verify_payment(cost, addy)
+        with st.spinner('Scanning our store wallet...'):
+            time.sleep(3)
+        if res['result']:
+            st.balloons()
+            st.success(res['reason'], icon="✅")
         else:
-            st.error(res['reason'], icon='🛑')
+            if res['severity'] == 1:
+                st.warning(res['reason'], icon='⚠️')
+            else:
+                st.error(res['reason'], icon='🛑')
+    else:
+        st.error('Please complete the form', icon='🛑')
--- pages/3_GR86.py
+++ pages/3_GR86.py
@@ -1,16 +1,28 @@
 import streamlit as st
 from verification import verify_payment
+import time
 
 price = '35,000'
 st.text('GR86')
 st.image('https://i.ibb.co/j5BDZWJ/yoda.png')
 addy = st.text_input('Enter your purchasing address to confirm transaction')
+contact_type = st.selectbox('How would you like to arrange delivery', ('select', 'Email', 'Phone'))
+contact = st.text_input('Contact' if contact_type == 'select' else contact_type)
 st.text(price)
 btn = st.button('Confirm')
 if btn:
     cost = int(price.replace(',', ''))
-    res = verify_payment(cost, addy)
-    if res:
-        st.success('Your purchase has been confirmed! Delivery arrangements will be made shortly!')
+    if addy != '' and contact_type != 'select' and contact != '':
+        res = verify_payment(cost, addy)
+        with st.spinner('Scanning our store wallet...'):
+            time.sleep(3)
+        if res['result']:
+            st.balloons()
+            st.success(res['reason'], icon="✅")
+        else:
+            if res['severity'] == 1:
+                st.warning(res['reason'], icon='⚠️')
+            else:
+                st.error(res['reason'], icon='🛑')
     else:
-        st.warning('An error has occured. Consider trying again later or making sure your payment went through.')
+        st.error('Please complete the form', icon='🛑')

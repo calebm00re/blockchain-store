093be2fdea69d21daf7b8aee5402660f8de01306
Caleb Moore <ctmoore@smu.edu>
2022-12-07 20:50:21 -0600

    Update 1_Jeep.py

--- pages/1_Jeep.py
+++ pages/1_Jeep.py
@@ -3,7 +3,7 @@ from verification import verify_payment
 import time
 import webbrowser
 
-price = '1,000'
+price = '50,000'
 st.text('Jeep')
 st.image('https://techcrunch.com/wp-content/uploads/2022/09/JP023_279WR5jvo53mde21dbrr902p5rahl1e.jpg')
 addy = st.text_input('Enter your purchasing address to confirm transaction')
